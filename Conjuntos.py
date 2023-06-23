44f79b4a6ae51572d3e09c92b8ce16a7638d1102
FilipeUhlig <136847439+FilipeUhlig@users.noreply.github.com>
2023-06-23 15:52:07 -0300

    Add files via upload

---
+++ Conjuntos.py
@@ -0,0 +1,33 @@
+"""
+Programa: conjuntos
+Descrição: Este programa apresenta a estrutura de dados "conjuntos" em Python para fins de aprendizado.
+Autor: F
+Data : 23/06/2023
+Versão: 0.0.1
+"""
+
+#Atribuição de variáveis
+
+lista = [1,2,3,3,5,7,9,11,31,8,8,47]
+conjunto1 = set(lista)
+conjunto2 = {1,3,3,5,5,0,0,23, -2}
+conjunto2
+
+#Entrada de dados
+
+
+
+
+#Processamento de dados
+
+conjunto2.add(49)
+conjunto2.remove(1)
+
+
+
+
+#Saida de dadaos
+
+print(conjunto1)
+print(lista)
+print(conjunto2)
